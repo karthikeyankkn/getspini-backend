82b32ceed92d37e959c60051b0830a7d8e12b04b
colkar99 <colkar99@gmail.com>
2018-06-25 15:42:57 +0530

    asdasd

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,12 +3,16 @@ lock "~> 3.10.2"
 
 set :application, "getspini-backend"
 set :repo_url, "https://github.com/karthikeyankkn/getspini-backend.git"
-set :deploy_to, "/home/ubuntu/var/backend"
+# set :deploy_to, "/home/ubuntu/var/backend"
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rvm_ruby_version, '2.4.1'
-set :rvm_type, :local
+set :rvm_type, :ubuntu
+# set :default_shell, :bash
+# set :passenger_environment_variables, { :path => '/path-to-passenger/bin:$PATH' }
 set :passenger_environment_variables, { :path => '/path-to-passenger/bin:$PATH' }
+# set :passenger_restart_with_touch, true
+# set :passenger_restart_command, '/path-to-passenger/bin/passenger-config restart-app'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
